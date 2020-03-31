805b374648bf5dc7a63455982ac60d208f67f0a3
Frannie Ziesemer <frances.ziesemer@gmail.com>
2020-03-31 21:56:59 +0200

    add navbar and about page, some styling

--- app/views.py
+++ app/views.py
@@ -1,8 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from app import app
 
+import requests, json
+
+navbar = [
+    {
+        'itemText': 'HOME',
+        'itemURL': '/'
+    },
+    {
+        'itemText': 'ABOUT US',
+        'itemURL': '/about'
+    },
+    {
+        'itemText': 'EVENTS',
+        'itemURL': '/events'
+    },
+    {
+        'itemText': 'CONTACT',
+        'itemURL': '/contact'
+    }
+]
 
 @app.route('/')
 def index():
     heading = "C'est Cheese on the Go"
-    return render_template('index.html', heading=heading)
+    return render_template('index.html', heading=heading, navbar=navbar)
+
+@app.route('/about')
+def about():
+    heading = "About"
+    return render_template('about.html', heading=heading, navbar=navbar)
+
+@app.route('/events')
+def events():
+    heading = "Events"
+    return render_template('events.html', heading=heading, navbar=navbar)
+
+@app.route('/contact')
+def contact():
+    heading = "Conact"
+    return render_template('contact.html', heading=heading, navbar=navbar)
